62fc9bdb04a4f80a1db3b18bb4b25655646b86a9
pervenchebeorou <beoroupervenche@gmail.com>
2025-04-02 13:41:20 +0000

    MAJ

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const swiper = new Swiper('.swiper', {
-    loop: true,
-    pagination: { el: '.swiper-pagination', clickable: true },
-    navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+    loop: true, // Permet de faire défiler de manière infinie
+    pagination: { 
+        el: '.swiper-pagination', // Pagination sous forme de points
+        clickable: true 
+    },
+    navigation: { 
+        nextEl: '.swiper-button-next', // Bouton suivant
+        prevEl: '.swiper-button-prev'  // Bouton précédent
+    }
 });
 
+
+// Fonction pour ouvrir l'image en plein écran
 function openFullscreen(img) {
     const overlay = document.getElementById('fullscreenOverlay');
     const fullscreenImage = document.getElementById('fullscreenImage');
     
-    fullscreenImage.src = img.src; 
-    overlay.style.display = 'flex'; 
+    fullscreenImage.src = img.src; // Affiche l'image sélectionnée
+    overlay.style.display = 'flex'; // Affiche l'overlay
 }
 
+// Fonction pour fermer l'overlay
 function closeFullscreen() {
     document.getElementById('fullscreenOverlay').style.display = 'none';
 }
